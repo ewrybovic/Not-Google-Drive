4b0ad0c64215cddd237b72f39e140bbbe4d52848
ewrybovic <42895807+ewrybovic@users.noreply.github.com>
2019-04-28 11:38:16 -0700

    Added search bar functionality

--- php/profile.php
+++ php/profile.php
@@ -16,13 +16,46 @@ echo $_SESSION['login_pass'];
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
+<input type="text" id="input" onkeyup="SearchFunction()"><br/>
+<ul id="list">
 <?php
   //user directory name files;
-  $files = scandir("../users/");
+  $files = scandir("../users/".$_SESSION['login_user']);
   for ($i=2; $i < count($files) ; $i++) {
     // add $user in session
-    echo "<p><a href= \"../users/" .$files[$i]. "\"download>" . $files[$i]. "</a>";
+    echo "<li><a href= \"../users/" .$files[$i]. "\"download>" . $files[$i]. "</a></li><br/>";
   }
 ?>
+</ul>
 </head>
 </html>
+<script type="text/javascript">
+
+function SearchFunction()
+{
+
+  // Get input from textbox
+  var input = document.getElementById('input').value;
+  list = document.getElementById('list').getElementsByTagName('li');
+  if (input)
+  {
+    //list.innerHTML += jsonData;
+    //for(var i = 0; i < filenames.length; ++i)
+    //elements.forEach(function(filename)
+    for(i =0; i < list.length; i++)
+    {
+      filename = list[i].getElementsByTagName("a")[0];
+      filename = filename.textContent || filename.innerHTML;
+      // Check if input is in the filename
+      if (filename.indexOf(input) > -1)
+      {
+        list[i].style.display = "";
+      }
+      else
+      {
+        list[i].style.display = "none";
+      }
+    }
+  }
+}
+</script>
